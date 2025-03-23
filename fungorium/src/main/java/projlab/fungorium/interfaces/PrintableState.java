4db4bd95bd4872502d2eaa65f882d789b7d75dd0
Szmoleniczki Ákos <szmoleniczki.akos@edu.bme.hu>
2025-03-23 17:10:47 +0100

    added doc comments

--- fungorium/src/main/java/projlab/fungorium/interfaces/PrintableState.java
+++ fungorium/src/main/java/projlab/fungorium/interfaces/PrintableState.java
@@ -1,5 +1,8 @@
 package projlab.fungorium.interfaces;
 
+/**
+ * Az állapottal rendelkező osztályoktól le lehet kérni az állapotukat Stringként, ha megvalósítják az interfacet.
+ */
 public interface PrintableState {
     public String getStateString();
 }
