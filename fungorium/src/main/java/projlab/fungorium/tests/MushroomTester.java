24753d8247c225fd2650f63e9b93004aba87a6a8
Zagyvai Peter <zagyvaipeter@gmail.com>
2025-03-23 18:30:33 +0100

    Added comments

--- fungorium/src/main/java/projlab/fungorium/tests/MushroomTester.java
+++ fungorium/src/main/java/projlab/fungorium/tests/MushroomTester.java
@@ -12,20 +12,29 @@ public class MushroomTester {
     private static MushroomThread mt1, mt2, mt3, mt4, mt5, normalThread, evolvingThread, dissolvingThread, dyingThread;
     private static MushroomBody mb, mb1, mb2, advnacedBody, normalBody, lastSporeBody;
 
-    private static void cutConnectionOnyOneBodyInit() {
+    /**
+     * Létrehoz 3 tectont, amik lánc szerűen szomszédosakegymással. <p>
+     * Ezekre felhelyez 1-1 gomba fonalat, amiket összeköt egymással. <p>
+     * Végül az egyik szélső tektonra elhelyez egy gombatestet. <p>
+     */
+    private static void cutConnectionOnlyOneBodyInit() {
+        // Tektonok inicializációja
         t1 = new Tecton();
         t2 = new Tecton();
         t3 = new Tecton();
 
+        // Szomszédok megadása
         t1.registerNeighbour(t2);
 
         t2.registerNeighbour(t1);
         t2.registerNeighbour(t3);
         
         t3.registerNeighbour(t2);
-
+        
+        // Első gomba fonal létrehozása
         mt3 = new MushroomThread(t3);
-
+        
+        // Tőbbi gombafonál összekötése
         try {
             mt2 = mt3.createConnection(t2);
             mt1 = mt2.createConnection(t1);
@@ -33,16 +42,24 @@ public class MushroomTester {
             Logger.printError(e.getMessage());
         }
 
+        // Gombatest elhelyezése
         mb = new MushroomBody(t3);
     }
 
+    /**
+     * Létrehoz 5 tektont, amik lánc szerűen szomszédosak egymással. <p>
+     * Ezekre felhelyez 1-1 gomba fonalat, amiket összeköt egymással. <p>
+     * Végül a két szélső tektonra elhelyez 1-1 gombatestet.
+     */
     private static void cutConnectionWithTwoBodiesInit() {
+        // Tektonok inicializálása
         t1 = new Tecton();
         t2 = new Tecton();
         t3 = new Tecton();
         t4 = new Tecton();
         t5 = new Tecton();
 
+        // Szomszédok beállítása
         t1.registerNeighbour(t2);
 
         t2.registerNeighbour(t1);
@@ -56,11 +73,14 @@ public class MushroomTester {
 
         t5.registerNeighbour(t4);
 
+        // Gombatestek felhelyezése
         mb1 = new MushroomBody(t5);
         mb2 = new MushroomBody(t1);
 
+        // Első gombafonal inicializálása
         mt5 = new MushroomThread(t5);
 
+        // Gombafonalak kialakítása kapcsolatokkal
         try {
             mt4 = mt5.createConnection(t1);
             mt3 = mt4.createConnection(t1);
@@ -72,11 +92,20 @@ public class MushroomTester {
     }
     
 
+    /**
+     * Létrehoz három tektont, amik láncszerűen szomszédosak egymással. <p>
+     * Mindhárom tektonra felhelyez egy-egy gombatestet: <p>
+     * - Egyik gombatest "advanced" típsú <p>
+     * - Másik gombatest "normal" típusú <p>
+     * - Harmadik szintén "normal" típusú viszont, már csak egy spórát tud elszórni
+     */
     private static void mushroomBodyTestInit() {
+        // Tektonok inicializálása
         t1 = new Tecton();
         t2 = new Tecton();
         t3 = new Tecton();
 
+        // Szomszédok beállítása
         t1.registerNeighbour(t2);
 
         t2.registerNeighbour(t1);
@@ -84,6 +113,7 @@ public class MushroomTester {
 
         t3.registerNeighbour(t2);
 
+        // Gombatestek felhelyezése
         advnacedBody = new MushroomBody(t1);
         advnacedBody.setAdvancement(Advancement.ADVANCED);
 
@@ -93,12 +123,24 @@ public class MushroomTester {
         lastSporeBody.setSporesRemaining(1);
     }
 
+    /**
+     * Létrehoz két tektont, amik nem szomszédosak. <p>
+     * Ezekre felhelyez 2-2 gombafonalat és a két tekton közül az egyikre egy gombatestet. <p>
+     * Gombafonalak jellemzői: <p>
+     * - normal gombafonál (semmit nem változtat rajta az inicializálás során; kapcsolatban van gombatesttel). <p>
+     * - evolving gombafonál (következő alkalommal, ha növekszik, akkor SPROUT állapotból GROWN állapotba lép; kapcsolatban van gombatesttel). <p>
+     * - dissolving gombafonál (ugyan olyan mint a normal gombafonál, viszont nincs kapcsolatban gombatesttel). <p>
+     * - dying gombafonál (turnsToDie változója 1, a következő endOfTheRound hívással el kell tűnnie a tektonról; nincs kapcsolatban gombatesttel). 
+     */
     private static void mushroomThreadTestInit() {
+        // Tektonok inicializálása
         t1 = new Tecton();
         t2 = new Tecton();
 
+        // Gombatest inicializálása
         mb = new MushroomBody(t1);
 
+        // Gombafonalak inicializálása
         normalThread = new MushroomThread(t1);
 
         evolvingThread = new MushroomThread(t1);
@@ -111,6 +153,12 @@ public class MushroomTester {
         dyingThread.setTurnsToDie(1);
     }
 
+
+    /**
+     * Létrehoz kettő tektont, amik szomszédosak egymással. <p>
+     * Mindkét tektonra felvesz egy-egy gombafonalat, amik össze vannak kötve egymással. <p>
+     * Beállítja, hogy a fonalak "GROWN" állapotban legyenek, majd a kettő közül az egyiket elvágja. <p>
+     */
     private static void threadIsConnectingTectonFailCutInit() {
         t1 = new Tecton();
         t2 = new Tecton();
@@ -131,6 +179,11 @@ public class MushroomTester {
         mt1.cut();
     }
 
+    /**
+     * Létrehoz kettő tektont, amik szomszédosak egymással. <p>
+     * Mindkét tektonra felvesz egy-egy gombafonalat, amik össze vannak kötve egymással. <p>
+     * Beállítja, hogy a fonalak "GROWN" állapotban legyenek. <p>
+     */
     private static void threadIsConnectingTectonSuccessInit() {
         t1 = new Tecton();
         t2 = new Tecton();
@@ -149,6 +202,11 @@ public class MushroomTester {
         mt2.setGrowState(GrowState.GROWN);
     }
 
+    /**
+     * Létrehoz kettő tektont, amik szomszédosak egymással. <p>
+     * Mindkét tektonra felvesz egy-egy gombafonalat, amik össze vannak kötve egymással. <p>
+     * Beállítja, hogy az egyik fonal "SPROUT" állapotban legyen. <p>
+     */
     private static void threadIsConnectingTectonFailSproutInit() {
         t1 = new Tecton();
         t2 = new Tecton();
@@ -166,64 +224,100 @@ public class MushroomTester {
         mt1.setGrowState(GrowState.SPROUT);
     }
 
+    /**
+     * Egy "advanced" gombatest spóra szórását vizsgálja 
+     */
     public static void test_advancedMushroomDstibuteSpores() {
         mushroomBodyTestInit();
         advnacedBody.distributeSpores();
     }
 
+    /**
+     * Vizsgálja, hogy egy tekton össze van-e kötve gombafonállal, olyan esetben, ha a gombafonál el van vágva
+     */
     public static void test_connectingTectinFailCut() {
         threadIsConnectingTectonFailCutInit();
         mt1.isConnectingTecton(t2);
     }
 
+    /**
+     * Vizsgálja, hogy egy tekton össze van-e kötve gombafonállal, olyan esetben, ha a gombafonál még csak SPROUT állapotban van
+     */
     public static void test_connectingTectonFailSprout() {
         threadIsConnectingTectonFailSproutInit();
         mt1.isConnectingTecton(t2);
     }
 
+    /**
+     * Vizsgálja, hogy egy tekton össze van-e kötve gombafonállal
+     */
     public static void test_connectingTectonSuccess() {
         threadIsConnectingTectonSuccessInit();
         mt1.isConnectingTecton(t2);
     }
 
+    /**
+     * Egy olyan gombatest spóra szórását vizsgálja, aminek már csak 1 spórája maradt
+     */
     public static void test_lastSpore() {
         mushroomBodyTestInit();
         lastSporeBody.distributeSpores();
     }
 
+    /**
+     * Vizsgálja, hogy mi történik egy olyan gombafonállal a körök végén, ami már olyan rég óta nem volt kapcsolatban gombatesttel, hogy ennek a körnek a végén fel kell szívódnia.
+     */
     public static void test_threadDie() {
         mushroomThreadTestInit();
         dyingThread.onEndOfTheRound();
     }
 
+    /**
+     * Vizsgálja, hogy mi történik egy olyan gombafonállal a körök végén, ami nincs kapcsolatban gombatesttel, de még nem olyan rég óta hogy fel kelljen szívódnia.
+     */
     public static void test_threadDissolve() {
         mushroomThreadTestInit();
         dissolvingThread.onEndOfTheRound();
     }
 
+    /**
+     * Vizsgálja, hogy mi történik egy olyan gombafonállal a körök végén, ami kapcsolatban van gombafonállal és már elég rég óta él ahhoz, hogy SPROUT-ból GROWN állapotba lépjen
+     */
     public static void test_threadEvolve() {
         mushroomThreadTestInit();
         evolvingThread.onEndOfTheRound();
     }
 
+    /**
+     * Vizsgálja, hogy mi történik egy olyan gombafonállal a körök végén, ami kapcsolatban van gombafonállal de még nem él elég rég óta ahhoz, hogy SPROUT-ból GROWN állapotba lépjen
+     */
     public static void test_threadGrowth() {
         mushroomThreadTestInit();
         normalThread.onEndOfTheRound();
     }
 
+    /**
+     * Egy "normal" gombatest spóraszórását vizsgálja 
+     */
     public static void test_normalBodyDistibuteSpores() {
         mushroomBodyTestInit();
         normalBody.distributeSpores();
     }
 
+    /**
+     * Azt vizsgálja, hogy mi történik, amikor egy olyan gombafonál-láncot vágnak el, aminek csak az egyik felén volt gomba test
+     */
     public static void test_cutConncectionOneBody() {
-        cutConnectionOnyOneBodyInit();
+        cutConnectionOnlyOneBodyInit();
         mt2.cut();
         mt1.isConnectedToBody();
         mt2.isConnectedToBody();
         mt3.isConnectedToBody();
     }
 
+    /**
+     * Azt vizsgálja, hogy mi történik, amikor egy olyan gombafonál-láncot vágnak el, aminek mindkét felén volt gomba test
+     */
     public static void test_cutConnectionTwoBodies() {
         cutConnectionWithTwoBodiesInit();
         mt3.cut();
