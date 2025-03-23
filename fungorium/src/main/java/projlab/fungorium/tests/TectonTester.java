6608cbc3ffc826cf2ff7c239b31efdec7b07c8f2
Szmoleniczki Ákos <szmoleniczki.akos@edu.bme.hu>
2025-03-23 16:38:32 +0100

    implemented tests as static methods without state logging yet

--- fungorium/src/main/java/projlab/fungorium/tests/TectonTester.java
+++ fungorium/src/main/java/projlab/fungorium/tests/TectonTester.java
@@ -11,270 +11,213 @@ import projlab.fungorium.models.MushroomThread;
 import projlab.fungorium.models.SingleThreadTecton;
 import projlab.fungorium.models.Tecton;
 import projlab.fungorium.models.ThreadKillingTecton;
+import projlab.fungorium.utilities.Logger;
 
 /**
  * Ez az osztály felelős a Tectonhoz tartozó tesztesetekért és azok inicializásáért.
  */
 public final class TectonTester {
     // -------------------------------------
-    // Init függvények
-
-    /**
-     * Beállítja a helyes kezdőállapotot a Tecton Split és a Tecton Kill Thread tesztekhez. (tecton1 comm diagram)
-     */
-    public final void initTests1() {
-        // unused
-        t2 = null;
-        mt2 = null;
-        ms1 = null;
-        ms2 = null;
-        ms3 = null;
-
-        // used
-        i = new Insect();
-        mt1 = new MushroomThread();
-        mt2 = new MushroomThread();
-        mb = new MushroomBody();
-        t1 = new Tecton(null, List.of(mt1, mt2), null, List.of(i), mb);
-        mt1.createConnection(t1); // ez nem tudom, hogy kell-e egyáltalán?
-        mt2.createConnection(t1); // ez nem tudom, hogy kell-e egyáltalán?
-    }
-
-    /**
-     * Beállítja a helyes kezdőállapotot a Thread Killing Tecton Kill Thread teszthez. (tecton2 comm diagram)
-     */
-    public final void initTests2() {
-        // unused
-        t2 = null;
-        mt2 = null;
-        i = null;
-        mb = null;
-        ms1 = null;
-        ms2 = null;
-        ms3 = null;
-
-        // used
-        mt1 = new MushroomThread();
-        t1 = new ThreadKillingTecton(null, List.of(mt1), null, null, null);
-        mt1.createConnection(t1); // ez nem tudom, hogy kell-e egyáltalán?
-    }
-
-    /**
-     * Beállítja a helyes kezdőállapotot a Tecton Grow Thread Success és a Tecton Grow Body Success tesztekhez. (tecton3_1 comm diagram)
-     */
-    public final void initTests3_1() {
-        // unused
-        mt2 = null;
-        i = null;
-        mb = null;
-
-        // used
-        ms1 = new MushroomSpore();
-        ms2 = new MushroomSpore();
-        ms3 = new MushroomSpore();
-        mt1 = new MushroomThread();
-        t2 = new Tecton();
-        t1 = new Tecton(List.of(t2), List.of(mt1), List.of(ms1, ms2, ms3), null, null);
-        mt1.createConnection(t1); // TODO: ez nem tudom, hogy kell-e egyáltalán?
-    }
-
-    /**
-     * Beállítja a helyes kezdőállapotot a Tecton Grow Thread Fail teszthez. (tecton3_2 comm diagram)
-     */
-    public final void initTests3_2() {
-        // unused
-        mt2 = null;
-        mb = null;
-        i = null;
-        ms1 = null;
-        ms2 = null;
-        ms3 = null;
-
-        // used
-        mt1 = new MushroomThread();
-        t1 = new Tecton(null, List.of(mt1), null, null, null);
-        mt1.createConnection(t1); // TODO: ez nem tudom, hogy kell-e egyáltalán?
-        t2 = new Tecton();
-    }
-
-    /**
-     * Beállítja a helyes kezdőállapotot a Tecton Grow Body Thread Fail és a Tecton Grow Body Spore Fail tesztekhez. (tecton3_3 comm diagram)
-     */
-    public final void initTests3_3() {
-        // unused
-        mt2 = null;
-        i = null;
-        mb = null;
-
-        // used
-        ms1 = new MushroomSpore();
-        ms2 = new MushroomSpore();
-        ms3 = new MushroomSpore();
-        mt1 = new MushroomThread();
-        t2 = new Tecton(null, null, List.of(ms1, ms2, ms3), null, null);
-        t1 = new Tecton(List.of(t2), List.of(mt1), null, null, null);
-        mt1.createConnection(t1); // TODO: ez nem tudom, hogy kell-e egyáltalán?
-    }
-
-    /**
-     * Beállítja a helyes kezdőállapotot a Tecton Grow Body Body Fail teszthez. (tecton3_4 comm diagram)
-     */
-    public final void initTests3_4() {
-        // unused
-        t2 = null;
-        i = null;
-        mt2 = null;
-
-        // used
-        ms1 = new MushroomSpore();
-        ms2 = new MushroomSpore();
-        ms3 = new MushroomSpore();
-        mt1 = new MushroomThread();
-        mb = new MushroomBody();
-        t1 = new Tecton(null, List.of(mt1), List.of(ms1, ms2, ms3), null, mb);
-        mt1.createConnection(t1); // TODO: ez nem tudom, hogy kell-e egyáltalán?
-    }
-
-    /**
-     * Beállítja a helyes kezdőállapotot a SingleThreadTecton Grow Thread Success és a SingleThreadTecton Grow Thread Fail tesztekhez. (tecton4 comm diagram)
-     */
-    public final void initTests4() {
-        // unused
-        mt2 = null;
-        ms1 = null;
-        ms2 = null;
-        ms3 = null;
-        mb = null;
-        i = null;
-
-        // used
-        mt1 = new MushroomThread();
-        t2 = new SingleThreadTecton();
-        t1 = new SingleThreadTecton(List.of(t2), mt1, null, null, null);
-        mt1.createConnection(t1); // TODO: ez nem tudom, hogy kell-e egyáltalán?
-    }
-
-    /**
-     * Beállítja a helyes kezdőállapotot a InfertileTecton Grow Body Fail teszthez. (tecton5 comm diagram)
-     */
-    public final void initTests5() {
-        // unused
-        t2 = null;
-        mt1 = null;
-        mt2 = null;
-        mb = null;
-        i = null;
-        ms1 = null;
-        ms2 = null;
-        ms3 = null;
-
-
-        // used
-        t1 = new InfertileTecton();
-    }
-
-
-
-    // -------------------------------------
     // Teszt függvények
 
     /**
      * InfertileTecton Grow Body Fail teszteset megvalósítása.
      */
+    // TODO: init diagram rework
     public static final void InfertileTectonGrowBodyFail() {
+        // init
+        InfertileTecton t = new InfertileTecton();
+        MushroomThread mt1 = new MushroomThread(t);
+        MushroomSpore ms1 = new MushroomSpore(t);
+        MushroomSpore ms2 = new MushroomSpore(t);
+        MushroomSpore ms3 = new MushroomSpore(t);
 
+        // test
+        MushroomBody mb = new MushroomBody(t);
     }
 
     /**
      * SingleThreadTecton Grow Thread Fail teszteset megvalósítása.
      */
-    public final void SingleThreadTectonGrowThreadFail() {
+    // TODO: init diagram rework
+    public static final void SingleThreadTectonGrowThreadFail() {
+        // init
+        SingleThreadTecton t1 = new SingleThreadTecton();
+        SingleThreadTecton t2 = new SingleThreadTecton(List.of(t1));
+        SingleThreadTecton t3 = new SingleThreadTecton(List.of(t1, t2));
+        MushroomThread mt1 = new MushroomThread(t1);
+        MushroomThread mt2 = new MushroomThread(t2);
 
+        // test
+        try {
+            mt1.createConnection(t2);
+        } catch (Exception e) {
+            Logger.printError(e.getMessage());
+        }
     }
 
     /**
      * SingleThreadTecton Grow Thread Success teszteset megvalósítása.
      */
-    public final void SingleThreadTectonGrowThreadSuccess() {
+    public static final void SingleThreadTectonGrowThreadSuccess() {
+        // init
+        SingleThreadTecton t1 = new SingleThreadTecton();
+        SingleThreadTecton t2 = new SingleThreadTecton(List.of(t1));
+        SingleThreadTecton t3 = new SingleThreadTecton(List.of(t1, t2));
+        MushroomThread mt1 = new MushroomThread(t1);
+        MushroomThread mt2 = new MushroomThread(t2);
 
+        // test
+        try {
+            mt1.createConnection(t3);
+        } catch (Exception e) {
+            Logger.printError(e.getMessage());
+        }
     }
 
     /**
      * Tecton Grow Body Body Fail teszteset megvalósítása.
      */
-    public final void TectonGrowBodyBodyFail() {
+    public static final void TectonGrowBodyBodyFail() {
+        // init
+        Tecton t1 = new Tecton();
+        MushroomThread mt1 = new MushroomThread(t1);
+        MushroomSpore ms1 = new MushroomSpore(t1);
+        MushroomSpore ms2 = new MushroomSpore(t1);
+        MushroomSpore ms3 = new MushroomSpore(t1);
+        MushroomBody mb1 = new MushroomBody(t1);
 
+        // test
+        MushroomBody mb2 = new MushroomBody(t1);
     }
 
     /**
      * Tecton Grow Body Spore Fail teszteset megvalósítása.
      */
-    public final void TectonGrowBodySporeFail() {
+    // TODO: fix the body-thread situation
+    public static final void TectonGrowBodySporeFail() {
+        // init
+        Tecton t1 = new Tecton();
+        Tecton t2 = new Tecton(List.of(t1));
+        MushroomThread mt1 = new MushroomThread(t1);
+        MushroomSpore ms1 = new MushroomSpore(t2);
+        MushroomSpore ms2 = new MushroomSpore(t2);
+        MushroomSpore ms3 = new MushroomSpore(t2);
 
+        // test
+        MushroomBody mb = new MushroomBody(t1);
     }
 
     /**
      * Tecton Grow Body Success teszteset megvalósítása.
      */
-    public final void TectonGrowBodySuccess() {
+    // TODO: fix the body-thread situation
+    public static final void TectonGrowBodySuccess() {
+        // init
+        Tecton t1 = new Tecton();
+        Tecton t2 = new Tecton(List.of(t1));
+        MushroomThread mt1 = new MushroomThread(t1);
+        MushroomSpore ms1 = new MushroomSpore(t1);
+        MushroomSpore ms2 = new MushroomSpore(t1);
+        MushroomSpore ms3 = new MushroomSpore(t1);
 
+        // test
+        MushroomBody mb = new MushroomBody(t1);
     }
 
     /**
      * Tecton Grow Body Thread Fail teszteset megvalósítása.
      */
-    public final void TectonGrowBodyThreadFail() {
+    // TODO: fix the body-thread situation
+    public static final void TectonGrowBodyThreadFail() {
+        // init
+        Tecton t1 = new Tecton();
+        Tecton t2 = new Tecton(List.of(t1));
+        MushroomThread mt1 = new MushroomThread(t1);
+        MushroomSpore ms1 = new MushroomSpore(t2);
+        MushroomSpore ms2 = new MushroomSpore(t2);
+        MushroomSpore ms3 = new MushroomSpore(t2);
 
+        // test
+        MushroomBody mb = new MushroomBody(t2);
     }
 
     /**
      * Tecton Grow Thread Fail teszteset megvalósítása.
      */
-    public final void TectonGrowThreadFail() {
+    // TODO: ez nem ide tartozik, hanem inkabb a fonalhoz
+    // public static final void TectonGrowThreadFail() {
 
-    }
+    // }
 
     /**
      * Tecton Grow Thread Success teszteset megvalósítása.
      */
-    public final void TectonGrowThreadSuccess() {
+    // TODO: ez nem ide tartozik, hanem inkabb a fonalhoz
+    // public static final void tectonGrowThreadSuccess() {
+    //     // init
+    //     Tecton t1 = new Tecton();
+    //     Tecton t2 = new Tecton(List.of(t1));
+    //     MushroomThread mt1 = new MushroomThread(t1);
+    //     MushroomSpore ms1 = new MushroomSpore(t1);
+    //     MushroomSpore ms2 = new MushroomSpore(t1);
+    //     MushroomSpore ms3 = new MushroomSpore(t1);
 
-    }
+    //     // test
+    //     Logger.print("void", "Tecton::addConnection", "");
+    // }
 
     /**
      * Tecton Kill Thread teszteset megvalósítása.
      */
-    public final void TectonKillThread() {
+    public static final void tectonKillThread() {
+        // init
+        Tecton t = new Tecton();
+        Insect i = new Insect(t);
+        MushroomThread mt1 = new MushroomThread(t);
+        MushroomThread mt2 = new MushroomThread(t);
+        MushroomBody mb = new MushroomBody(t);
 
+        // test
+        Logger.print("void", "Tecton::killThreads", "");
+        try {
+            t.killThreads();
+        } 
+        catch (Exception e) {
+            Logger.printError(e.getMessage());
+        }
     }
 
     /**
      * Tecton Split teszteset megvalósítása.
      */
-    public final void TectonSplit() {
+    public static final void tectonSplit() {
+        // init
+        Tecton t = new Tecton();
+        Insect i = new Insect(t);
+        MushroomThread mt1 = new MushroomThread(t);
+        MushroomThread mt2 = new MushroomThread(t);
+        MushroomBody mb = new MushroomBody(t);
+
+        // test
+        
+
+        Logger.print("void", "Tecton::split", "");
+        t.split();
+
 
     }
 
     /**
      * ThreadKillingTecton Kill Thread teszteset megvalósítása.
      */
-    public final void ThreadKillingTectonKillThread() {
+    public static final void threadKillingTectonKillThread() {
+        // init
+        ThreadKillingTecton t = new ThreadKillingTecton();
+        MushroomThread mt = new MushroomThread(t);
 
+        // test
+        Logger.print("void", "ThreadKillingTecton::killThreads", "");
+        t.killThreads();
     }
-
-
-
-
-    // -------------------------------------
-    // State
-
-    private Tecton t1;
-    private Tecton t2;
-    private Insect i;
-    private MushroomThread mt1;
-    private MushroomThread mt2;
-    private MushroomBody mb;
-    private MushroomSpore ms1;
-    private MushroomSpore ms3;
-    private MushroomSpore ms2;
 }
