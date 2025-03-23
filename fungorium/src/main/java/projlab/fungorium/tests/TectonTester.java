2f0426a66cc358b354d34376b1a38938fdf7f908
Szmoleniczki Ákos <szmoleniczki.akos@edu.bme.hu>
2025-03-23 10:59:37 +0100

    added framework for tecton tests

--- fungorium/src/main/java/projlab/fungorium/tests/TectonTester.java
+++ fungorium/src/main/java/projlab/fungorium/tests/TectonTester.java
@@ -1,5 +1,176 @@
 package projlab.fungorium.tests;
 
-public class TectonTester {
-    
+import projlab.fungorium.models.Insect;
+import projlab.fungorium.models.MushroomBody;
+import projlab.fungorium.models.MushroomSpore;
+import projlab.fungorium.models.MushroomThread;
+import projlab.fungorium.models.Tecton;
+
+/**
+ * Ez az osztály felelős a Tectonhoz tartozó tesztesetekért és azok inicializásáért.
+ */
+public final class TectonTester {
+    // -------------------------------------
+    // Init függvények
+
+    /**
+     * Beállítja a helyes kezdőállapotot a Tecton Split és a Tecton Kill Thread tesztekhez.
+     */
+    public final void initTests1() {
+
+    }
+
+    /**
+     * Beállítja a helyes kezdőállapotot a Thread Killing Tecton Kill Thread teszthez.
+     */
+    public final void initTests2() {
+
+    }
+
+    /**
+     * Beállítja a helyes kezdőállapotot a Tecton Grow Thread Success és a Tecton Grow Body Success tesztekhez.
+     */
+    public final void initTests3_1() {
+
+    }
+
+    /**
+     * Beállítja a helyes kezdőállapotot a Tecton Grow Thread Fail teszthez.
+     */
+    public final void initTests3_2() {
+
+    }
+
+    /**
+     * Beállítja a helyes kezdőállapotot a Tecton Grow Body Thread Fail és a Tecton Grow Body Spore Fail tesztekhez.
+     */
+    public final void initTests3_3() {
+
+    }
+
+    /**
+     * Beállítja a helyes kezdőállapotot a Tecton Grow Body Body Fail teszthez.
+     */
+    public final void initTests3_4() {
+
+    }
+
+    /**
+     * Beállítja a helyes kezdőállapotot a SingleThreadTecton Grow Thread Success és a SingleThreadTecton Grow Thread Fail tesztekhez.
+     */
+    public final void initTests4() {
+
+    }
+
+    /**
+     * Beállítja a helyes kezdőállapotot a InfertileTecton Grow Body Fail teszthez.
+     */
+    public final void initTests5() {
+
+    }
+
+
+
+    // -------------------------------------
+    // Teszt függvények
+
+    /**
+     * InfertileTecton Grow Body Fail teszteset megvalósítása.
+     */
+    public final void InfertileTectonGrowBodyFail() {
+
+    }
+
+    /**
+     * SingleThreadTecton Grow Thread Fail teszteset megvalósítása.
+     */
+    public final void SingleThreadTectonGrowThreadFail() {
+
+    }
+
+    /**
+     * SingleThreadTecton Grow Thread Success teszteset megvalósítása.
+     */
+    public final void SingleThreadTectonGrowThreadSuccess() {
+
+    }
+
+    /**
+     * Tecton Grow Body Body Fail teszteset megvalósítása.
+     */
+    public final void TectonGrowBodyBodyFail() {
+
+    }
+
+    /**
+     * Tecton Grow Body Spore Fail teszteset megvalósítása.
+     */
+    public final void TectonGrowBodySporeFail() {
+
+    }
+
+    /**
+     * Tecton Grow Body Success teszteset megvalósítása.
+     */
+    public final void TectonGrowBodySuccess() {
+
+    }
+
+    /**
+     * Tecton Grow Body Thread Fail teszteset megvalósítása.
+     */
+    public final void TectonGrowBodyThreadFail() {
+
+    }
+
+    /**
+     * Tecton Grow Thread Fail teszteset megvalósítása.
+     */
+    public final void TectonGrowThreadFail() {
+
+    }
+
+    /**
+     * Tecton Grow Thread Success teszteset megvalósítása.
+     */
+    public final void TectonGrowThreadSuccess() {
+
+    }
+
+    /**
+     * Tecton Kill Thread teszteset megvalósítása.
+     */
+    public final void TectonKillThread() {
+
+    }
+
+    /**
+     * Tecton Split teszteset megvalósítása.
+     */
+    public final void TectonSplit() {
+
+    }
+
+    /**
+     * ThreadKillingTecton Kill Thread teszteset megvalósítása.
+     */
+    public final void ThreadKillingTectonKillThread() {
+
+    }
+
+
+
+
+    // -------------------------------------
+    // State
+
+    private Tecton t1;
+    private Tecton t2;
+    private Insect i;
+    private MushroomThread mt1;
+    private MushroomThread mt2;
+    private MushroomBody mb;
+    private MushroomSpore ms1;
+    private MushroomSpore ms3;
+    private MushroomSpore ms2;
 }
