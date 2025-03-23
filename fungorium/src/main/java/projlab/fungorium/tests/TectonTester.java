3e7db0d537e82eb487b1453b17a4adbd4ab9d5c0
Szmoleniczki Ákos <szmoleniczki.akos@edu.bme.hu>
2025-03-23 11:43:48 +0100

    implemented init methods for the tests

--- fungorium/src/main/java/projlab/fungorium/tests/TectonTester.java
+++ fungorium/src/main/java/projlab/fungorium/tests/TectonTester.java
@@ -1,10 +1,16 @@
 package projlab.fungorium.tests;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import projlab.fungorium.models.InfertileTecton;
 import projlab.fungorium.models.Insect;
 import projlab.fungorium.models.MushroomBody;
 import projlab.fungorium.models.MushroomSpore;
 import projlab.fungorium.models.MushroomThread;
+import projlab.fungorium.models.SingleThreadTecton;
 import projlab.fungorium.models.Tecton;
+import projlab.fungorium.models.ThreadKillingTecton;
 
 /**
  * Ez az osztály felelős a Tectonhoz tartozó tesztesetekért és azok inicializásáért.
@@ -14,59 +20,157 @@ public final class TectonTester {
     // Init függvények
 
     /**
-     * Beállítja a helyes kezdőállapotot a Tecton Split és a Tecton Kill Thread tesztekhez.
+     * Beállítja a helyes kezdőállapotot a Tecton Split és a Tecton Kill Thread tesztekhez. (tecton1 comm diagram)
      */
     public final void initTests1() {
+        // unused
+        t2 = null;
+        mt2 = null;
+        ms1 = null;
+        ms2 = null;
+        ms3 = null;
 
+        // used
+        i = new Insect();
+        mt1 = new MushroomThread();
+        mt2 = new MushroomThread();
+        mb = new MushroomBody();
+        t1 = new Tecton(null, List.of(mt1, mt2), null, List.of(i), mb);
+        mt1.createConnection(t1); // ez nem tudom, hogy kell-e egyáltalán?
+        mt2.createConnection(t1); // ez nem tudom, hogy kell-e egyáltalán?
     }
 
     /**
-     * Beállítja a helyes kezdőállapotot a Thread Killing Tecton Kill Thread teszthez.
+     * Beállítja a helyes kezdőállapotot a Thread Killing Tecton Kill Thread teszthez. (tecton2 comm diagram)
      */
     public final void initTests2() {
+        // unused
+        t2 = null;
+        mt2 = null;
+        i = null;
+        mb = null;
+        ms1 = null;
+        ms2 = null;
+        ms3 = null;
 
+        // used
+        mt1 = new MushroomThread();
+        t1 = new ThreadKillingTecton(null, List.of(mt1), null, null, null);
+        mt1.createConnection(t1); // ez nem tudom, hogy kell-e egyáltalán?
     }
 
     /**
-     * Beállítja a helyes kezdőállapotot a Tecton Grow Thread Success és a Tecton Grow Body Success tesztekhez.
+     * Beállítja a helyes kezdőállapotot a Tecton Grow Thread Success és a Tecton Grow Body Success tesztekhez. (tecton3_1 comm diagram)
      */
     public final void initTests3_1() {
+        // unused
+        mt2 = null;
+        i = null;
+        mb = null;
 
+        // used
+        ms1 = new MushroomSpore();
+        ms2 = new MushroomSpore();
+        ms3 = new MushroomSpore();
+        mt1 = new MushroomThread();
+        t2 = new Tecton();
+        t1 = new Tecton(List.of(t2), List.of(mt1), List.of(ms1, ms2, ms3), null, null);
+        mt1.createConnection(t1); // TODO: ez nem tudom, hogy kell-e egyáltalán?
     }
 
     /**
-     * Beállítja a helyes kezdőállapotot a Tecton Grow Thread Fail teszthez.
+     * Beállítja a helyes kezdőállapotot a Tecton Grow Thread Fail teszthez. (tecton3_2 comm diagram)
      */
     public final void initTests3_2() {
+        // unused
+        mt2 = null;
+        mb = null;
+        i = null;
+        ms1 = null;
+        ms2 = null;
+        ms3 = null;
 
+        // used
+        mt1 = new MushroomThread();
+        t1 = new Tecton(null, List.of(mt1), null, null, null);
+        mt1.createConnection(t1); // TODO: ez nem tudom, hogy kell-e egyáltalán?
+        t2 = new Tecton();
     }
 
     /**
-     * Beállítja a helyes kezdőállapotot a Tecton Grow Body Thread Fail és a Tecton Grow Body Spore Fail tesztekhez.
+     * Beállítja a helyes kezdőállapotot a Tecton Grow Body Thread Fail és a Tecton Grow Body Spore Fail tesztekhez. (tecton3_3 comm diagram)
      */
     public final void initTests3_3() {
+        // unused
+        mt2 = null;
+        i = null;
+        mb = null;
 
+        // used
+        ms1 = new MushroomSpore();
+        ms2 = new MushroomSpore();
+        ms3 = new MushroomSpore();
+        mt1 = new MushroomThread();
+        t2 = new Tecton(null, null, List.of(ms1, ms2, ms3), null, null);
+        t1 = new Tecton(List.of(t2), List.of(mt1), null, null, null);
+        mt1.createConnection(t1); // TODO: ez nem tudom, hogy kell-e egyáltalán?
     }
 
     /**
-     * Beállítja a helyes kezdőállapotot a Tecton Grow Body Body Fail teszthez.
+     * Beállítja a helyes kezdőállapotot a Tecton Grow Body Body Fail teszthez. (tecton3_4 comm diagram)
      */
     public final void initTests3_4() {
+        // unused
+        t2 = null;
+        i = null;
+        mt2 = null;
 
+        // used
+        ms1 = new MushroomSpore();
+        ms2 = new MushroomSpore();
+        ms3 = new MushroomSpore();
+        mt1 = new MushroomThread();
+        mb = new MushroomBody();
+        t1 = new Tecton(null, List.of(mt1), List.of(ms1, ms2, ms3), null, mb);
+        mt1.createConnection(t1); // TODO: ez nem tudom, hogy kell-e egyáltalán?
     }
 
     /**
-     * Beállítja a helyes kezdőállapotot a SingleThreadTecton Grow Thread Success és a SingleThreadTecton Grow Thread Fail tesztekhez.
+     * Beállítja a helyes kezdőállapotot a SingleThreadTecton Grow Thread Success és a SingleThreadTecton Grow Thread Fail tesztekhez. (tecton4 comm diagram)
      */
     public final void initTests4() {
+        // unused
+        mt2 = null;
+        ms1 = null;
+        ms2 = null;
+        ms3 = null;
+        mb = null;
+        i = null;
 
+        // used
+        mt1 = new MushroomThread();
+        t2 = new SingleThreadTecton();
+        t1 = new SingleThreadTecton(List.of(t2), mt1, null, null, null);
+        mt1.createConnection(t1); // TODO: ez nem tudom, hogy kell-e egyáltalán?
     }
 
     /**
-     * Beállítja a helyes kezdőállapotot a InfertileTecton Grow Body Fail teszthez.
+     * Beállítja a helyes kezdőállapotot a InfertileTecton Grow Body Fail teszthez. (tecton5 comm diagram)
      */
     public final void initTests5() {
+        // unused
+        t2 = null;
+        mt1 = null;
+        mt2 = null;
+        mb = null;
+        i = null;
+        ms1 = null;
+        ms2 = null;
+        ms3 = null;
 
+
+        // used
+        t1 = new InfertileTecton();
     }
 
 
