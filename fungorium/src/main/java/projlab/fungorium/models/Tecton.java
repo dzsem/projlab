efedaaf0f609676d75dbb34aab951bc1008cbfdf
Szmoleniczki Ákos <145291965+szmoli@users.noreply.github.com>
2025-04-19 12:42:20 +0200

    added KeepAliveTecton

--- fungorium/src/main/java/projlab/fungorium/models/Tecton.java
+++ fungorium/src/main/java/projlab/fungorium/models/Tecton.java
@@ -1,12 +1,11 @@
 package projlab.fungorium.models;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import projlab.fungorium.interfaces.PrintableState;
 import projlab.fungorium.interfaces.TurnAware;
-
-import java.util.ArrayList;
 
 /**
  * A Tecton modellje.
@@ -302,6 +301,10 @@ public class Tecton implements TurnAware, PrintableState {
      */
     public void killThreads() throws Exception {
         throw new Exception("Non-ThreadKillingTectons can't kill MushroomThreads");
+    }
+
+    public void keepThreadsAlive() throws Exception {
+        throw new Exception("Non-KeepAliveTectons can't keep MushroomThreads alive");
     }
 
 
