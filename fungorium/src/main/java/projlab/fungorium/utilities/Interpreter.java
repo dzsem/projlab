40eba5b5f0852c233db0b599fbcb628e31fddde3
Paál Péter Ferenc <paalpeter04@gmail.com>
2025-04-27 16:55:33 +0200

    made config lang more readable, split it into separate functions

--- fungorium/src/main/java/projlab/fungorium/utilities/Interpreter.java
+++ fungorium/src/main/java/projlab/fungorium/utilities/Interpreter.java
@@ -31,206 +31,192 @@ public class Interpreter {
     private Map<String, InterpreterCommand> inputmap = new HashMap<>();
 
     private void init() {
-
+        inputmap.put("LOAD", args -> {
+            load(args.get(0));
+        });
+        inputmap.put("SAVE", args -> {
+            save(args.get(0));
+        });
+        inputmap.put("LIST", args -> {
+            list(args.get(0));
+        });
+        inputmap.put("SHOW", args -> {
+            show(Integer.valueOf(args.get(0)));
+        });
+        inputmap.put("DISTRIBUTESPORES", args -> {
+            distributespores(Integer.valueOf(args.get(0)));
+        });
+        /*
+         * inputmap.put("EATINSECT", args -> {
+         * load(args);
+         * });
+         */
+        inputmap.put("GROWBODY", args -> {
+            growbody(Integer.valueOf(args.get(0)));
+        });
+        inputmap.put("GROW", args -> {
+            grow(Integer.valueOf(args.get(0)), Integer.valueOf(args.get(1)));
+        });
+        inputmap.put("MOVE", args -> {
+            move(Integer.valueOf(args.get(0)), Integer.valueOf(args.get(1)));
+        });
+        inputmap.put("EATSPORE", args -> {
+            eatspore(Integer.valueOf(args.get(0)));
+        });
+        inputmap.put("CUTTHREAD", args -> {
+            cutthread(Integer.valueOf(args.get(0)), Integer.valueOf(args.get(1)));
+        });
+        inputmap.put("NEXTROUND", args -> {
+            nextround();
+        });
     };
 
-    /*
-     * A konfig nyelv ADD parancsát megvalósító függvény.
-     * Az első argumentumként megadott objektumot adja hozzá a játéhoz, a többi
-     * argumentumot az objektum konstruktorának adja át.
-     * 
-     * @param args a parancsnak megadott argumentumok listája
-     */
-    private void add(List<String> args) {
-        switch (args.get(0).toLowerCase()) {
-            case "tecton":
-                Tecton t = new Tecton();
-                game.addObject(t);
-                System.out.println("Tecton added");
+    private void addTecton() {
+        Tecton t = new Tecton();
+        game.addObject(t);
+        System.out.println("Tecton added");
+    }
+
+    private void addThreadkillingtecton() {
+        ThreadKillingTecton tkt = new ThreadKillingTecton();
+        game.addObject(tkt);
+        System.out.println("ThreadKillingTecton added");
+    }
+
+    private void addSinglethreadtecton() {
+        SingleThreadTecton stt = new SingleThreadTecton();
+        game.addObject(stt);
+        System.out.println("SingleThreadTecton added");
+    }
+
+    private void addInfertiletecton() {
+        InfertileTecton it = new InfertileTecton();
+        game.addObject(it);
+        System.out.println("InfertileTecton added");
+    }
+
+    private void addKeepalivetecton() {
+        KeepAliveTecton kat = new KeepAliveTecton();
+        game.addObject(kat);
+        System.out.println("KeepAliveTecton added");
+    }
+
+    private void addMushroomthread(List<String> args) {
+        Tecton threadtecton = (Tecton) game.getObject(Integer.valueOf(args.get(2)));
+        int threadmycid = Integer.valueOf(args.get(1));
+        MushroomThread mt = new MushroomThread(threadtecton, threadmycid);
+        game.addObject(mt);
+        System.out.println("MushroomThread added to Mycologist " + threadmycid + " and Tecton " + threadtecton.getID());
+    }
+
+    private void addMushroombody(List<String> args) {
+        Tecton bodytecton = (Tecton) game.getObject(Integer.valueOf(args.get(2)));
+        int bodymycid = Integer.valueOf(args.get(1));
+        MushroomBody mb = new MushroomBody(bodytecton, bodymycid);
+        game.addObject(mb);
+        System.out.println("MushroomBody added to Mycologist " + bodymycid + " and Tecton " + bodytecton.getID());
+    }
+
+    private void addMushroomspore(List<String> args) {
+        Tecton sporetecton = (Tecton) game.getObject(Integer.valueOf(args.get(1)));
+        MushroomSpore ms = new MushroomSpore(sporetecton);
+        game.addObject(ms);
+        System.out.println("MushroomSpore added to Tecton " + sporetecton.getID());
+    }
+
+    private void addInsect(List<String> args) {
+        Tecton insecttecton = (Tecton) game.getObject(Integer.valueOf(args.get(2)));
+        int insectologistid = Integer.valueOf(args.get(1));
+        Insect insect = new Insect(insectologistid, insecttecton);
+        game.addObject(insect);
+        System.out.println("Insect added to Insectologist " + insectologistid + " and Tecton " + insecttecton.getID());
+    }
+
+    private void registerNeighbour(List<String> args) {
+        GameObject t1 = game.getObject(Integer.valueOf(args.get(1)));
+        GameObject t2 = game.getObject(Integer.valueOf(args.get(2)));
+        if (t1 instanceof Tecton) {
+            if (t2 instanceof Tecton) {
+                ((Tecton) t1).registerNeighbour((Tecton) t2);
+            } else {
+                System.err.println("The second id does not belong to a Tecton.");
+            }
+        } else {
+            System.err.println("The first id does not belong to a Tecton.");
+        }
+    }
+
+    private void registerConnection(List<String> args) {
+        GameObject mt1 = game.getObject(Integer.valueOf(args.get(1)));
+        GameObject mt2 = game.getObject(Integer.valueOf(args.get(2)));
+        if (mt1 instanceof MushroomThread) {
+            if (mt2 instanceof MushroomThread) {
+                ((MushroomThread) mt1).addConnection((MushroomThread) mt2);
+                ((MushroomThread) mt2).addConnection((MushroomThread) mt1);
+            } else {
+                System.err.println("The second id does not belong to a MushroomThread.");
+            }
+        } else {
+            System.err.println("The first id does not belong to a MushroomThread.");
+        }
+    }
+
+    private void setTectonsplitchance(List<String> args) {
+        List<GameObject> gos = game.getGameObjects();
+        for (GameObject g : gos) {
+            if (g instanceof Tecton) {
+                ((Tecton) g).setSplitChance(Double.valueOf(args.get(1)) / 100);
+            }
+        }
+        System.out.println("Global Tecton split chance set to: " + args.get(1) + "%");
+    }
+
+    private void setTectonkillchance(List<String> args) {
+        List<GameObject> gos = game.getGameObjects();
+        for (GameObject g : gos) {
+            if (g instanceof ThreadKillingTecton) {
+                ((ThreadKillingTecton) g).setKillChance(Double.valueOf(args.get(1)) / 100);
+            }
+        }
+        System.out.println("ThreadKillingTecton kill chance set to: " + args.get(1) + "%");
+    }
+
+    private void setMushroomthread(List<String> args) {
+        MushroomThread mt = (MushroomThread) game.getObject(Integer.valueOf(args.get(2)));
+        if (!args.get(1).toLowerCase().equals("state")) {
+            System.err.println("Command not recognized.");
+            System.err.println("SET MUSHROOMTHREAD STATE <mushroomThreadID> <cutState> <growState>");
+            return;
+        }
+
+        switch (args.get(3).toLowerCase()) {
+            case "cut":
+                mt.setCutState(CutState.CUT);
                 break;
-            case "threadkillingtecton":
-                ThreadKillingTecton tkt = new ThreadKillingTecton();
-                game.addObject(tkt);
-                System.out.println("ThreadKillingTecton added");
-                break;
-            case "singlethreadtecton":
-                SingleThreadTecton stt = new SingleThreadTecton();
-                game.addObject(stt);
-                System.out.println("SingleThreadTecton added");
-                break;
-            case "infertiletecton":
-                InfertileTecton it = new InfertileTecton();
-                game.addObject(it);
-                System.out.println("InfertileTecton added");
-                break;
-            case "keepalivetecton":
-                KeepAliveTecton kat = new KeepAliveTecton();
-                game.addObject(kat);
-                System.out.println("KeepAliveTecton added");
-                break;
-            case "mushroomthread":
-                Tecton threadtecton = (Tecton) game.getObject(Integer.valueOf(args.get(2)));
-                int threadmycid = Integer.valueOf(args.get(1));
-                MushroomThread mt = new MushroomThread(threadtecton, threadmycid);
-                game.addObject(mt);
-                System.out.println(
-                        "MushroomThread added to Mycologist " + threadmycid + " and Tecton " + threadtecton.getID());
-                break;
-            case "mushroombody":
-                Tecton bodytecton = (Tecton) game.getObject(Integer.valueOf(args.get(2)));
-                int bodymycid = Integer.valueOf(args.get(1));
-                MushroomBody mb = new MushroomBody(bodytecton, bodymycid);
-                game.addObject(mb);
-                System.out.println(
-                        "MushroomBody added to Mycologist " + bodymycid + " and Tecton " + bodytecton.getID());
-                break;
-            case "mushroomspore":
-                Tecton sporetecton = (Tecton) game.getObject(Integer.valueOf(args.get(1)));
-                MushroomSpore ms = new MushroomSpore(sporetecton);
-                game.addObject(ms);
-                System.out.println(
-                        "MushroomSpore added to Tecton " + sporetecton.getID());
-                break;
-            case "insect":
-                Tecton insecttecton = (Tecton) game.getObject(Integer.valueOf(args.get(2)));
-                int insectologistid = Integer.valueOf(args.get(1));
-                Insect insect = new Insect(insectologistid, insecttecton);
-                game.addObject(insect);
-                System.out.println(
-                        "Insect added to Insectologist " + insectologistid + " and Tecton " + insecttecton.getID());
+            case "uncut":
+                mt.setCutState(CutState.UNCUT);
                 break;
             default:
-                System.err.println("Command not recognized. Possible parameters:");
-                System.err.println("ADD <tectontype>");
-                System.err.println("ADD MUSHROOMTHREAD <mycologistID> <tectonID>");
-                System.err.println("ADD MUSHROOMBODY <mycologistID> <tectonID>");
-                System.err.println("ADD MUSHROOMSPORE <mycologistID> <tectonID>");
-                System.err.println("ADD INSECT <insectologistID> <tectonID>");
+                System.err.println("Cut state not recognized. Possible states: CUT, UNCUT");
+                break;
+        }
+
+        switch (args.get(4).toLowerCase()) {
+            case "sprout":
+                mt.setGrowState(GrowState.SPROUT);
+                break;
+            case "grow":
+                mt.setGrowState(GrowState.GROWN);
+                break;
+            default:
+                System.err.println("Grow state not recognized. Possible states: GROW, SPROUT");
                 break;
         }
     }
 
-    /*
-     * A konfig nyelv REGISTER parancsát megvalósító függvény. Két Tecton vagy
-     * MushroomThread között létesít kapcsolatot.
-     * 
-     * @param args első eleme a kapcsolat típusa, a második és harmadik pedig a két
-     * objektum azonosítói
-     */
-    private void register(List<String> args) {
-        switch (args.get(0).toLowerCase()) {
-            case "neighbour":
-                GameObject t1 = game.getObject(Integer.valueOf(args.get(1)));
-                GameObject t2 = game.getObject(Integer.valueOf(args.get(2)));
-                if (t1 instanceof Tecton) {
-                    if (t2 instanceof Tecton) {
-                        ((Tecton) t1).registerNeighbour(((Tecton) t2));
-                    } else {
-                        System.err.println("The second id does not belong to a Tecton.");
-                    }
-                } else {
-                    System.err.println("The first id does not belong to a Tecton.");
-                }
-                break;
-            case "connection":
-                GameObject mt1 = game.getObject(Integer.valueOf(args.get(1)));
-                GameObject mt2 = game.getObject(Integer.valueOf(args.get(2)));
-                if (mt1 instanceof MushroomThread) {
-                    if (mt2 instanceof MushroomThread) {
-                        ((MushroomThread) mt1).addConnection((MushroomThread) mt2);
-                        ((MushroomThread) mt2).addConnection((MushroomThread) mt1);
-                    } else {
-                        System.err.println("The second id does not belong to a Tecton.");
-                    }
-                } else {
-                    System.err.println("The first id does not belong to a Tecton.");
-                }
-                break;
-            default:
-                System.err.println("Command not recognized. Possible parameters:");
-                System.err.println("REGISTER NEIGHBOUR <tectonID1> <tectonID2>");
-                System.err.println("REGISTER CONNECTION <mushroomThreadID1> <mushroomThreadID2>");
-                break;
-        }
-    }
-
-    /*
-     * A konfig nyelv SET parancsát megvalósító függvény. Be lehet vele állítani egy
-     * spóra effektjét, a tektonok szakadási esélyét, a fonálölő tekton fonálölési
-     * esélyét, és egy gombafonál vágási/növekedési állapotát.
-     * 
-     */
-    private void set(List<String> args) {
-        switch (args.get(0).toLowerCase()) {
-            /*
-             * TODO: after effectgeneration is implemented
-             * case "effectgeneration":
-             * 
-             * break;
-             */
-            case "tectonsplitchance":
-                List<GameObject> gos = game.getGameObjects();
-                for (int i = 0; i < gos.size(); i++) {
-                    GameObject g = gos.get(i);
-                    if (g instanceof Tecton) {
-                        ((Tecton) g).setSplitChance(Double.valueOf(args.get(1)) / 100);
-                    }
-                }
-                System.out.println("Global Tecton split chance set to: " + args.get(1) + "%");
-                break;
-            case "tectonkillchance":
-                List<GameObject> kgos = game.getGameObjects();
-                for (int i = 0; i < kgos.size(); i++) {
-                    GameObject g = kgos.get(i);
-                    if (g instanceof ThreadKillingTecton) {
-                        ((ThreadKillingTecton) g).setKillChance(Double.valueOf(args.get(1)) / 100);
-                    }
-                }
-                System.out.println("ThreadKillingTecton kill chance set to: " + args.get(1) + "%");
-                break;
-            case "mushroomthread":
-                MushroomThread mt = (MushroomThread) game.getObject(Integer.valueOf(args.get(2)));
-                if (!args.get(1).toLowerCase().equals("STATE")) {
-                    System.err.println("Command not recognized.");
-                    System.err.println("SET MUSHROOMTHREAD STATE <mushroomThreadID> <cutState> <growState>");
-                    break;
-                }
-                switch (args.get(2).toLowerCase()) {
-                    case "CUT":
-                        mt.setCutState(CutState.CUT);
-                        break;
-                    case "UNCUT":
-                        mt.setCutState(CutState.UNCUT);
-                        break;
-                    default:
-                        System.err.println("Cut state not recognized. Possible states: CUT, UNCUT");
-                        break;
-                }
-                switch (args.get(2).toLowerCase()) {
-                    case "SPROUT":
-                        mt.setGrowState(GrowState.SPROUT);
-                        break;
-                    case "GROW":
-                        mt.setGrowState(GrowState.GROWN);
-                        break;
-                    default:
-                        System.err.println("Grow state not recognized. Possible states: GROW, SPROUT");
-                        break;
-                }
-                break;
-            default:
-                System.err.println("Command not recognized. Possible parameters:");
-                System.err.println("SET EFFECTGENERATION <mushroomSporeID> <effect>");
-                System.err.println("SET TECTONSPLITCHANCE <chance>");
-                System.err.println("SET TECTONKILLCHANCE <chance>");
-                System.err.println("SET MUSHROOMTHREAD STATE <mushroomThreadID> <cutState> <growState>");
-                break;
-        }
-    }
-
-    private void load(List<String> args) {
+    private void load(String filename) {
         try {
-            BufferedReader br = new BufferedReader(new FileReader(args.get(0)));
+            BufferedReader br = new BufferedReader(new FileReader(filename));
             String fileline;
             while ((fileline = br.readLine()) != null) {
                 List<String> fl = Arrays.asList(fileline.split(" "));
@@ -252,15 +238,11 @@ public class Interpreter {
     }
 
     // TODO
-    private void save(File filename) {
-        for (GameObject obj : game.gameObjects) {
-            if (obj instanceof WritableGameObject writable) {
-                IOHandler.save(filename, writable);
-            }
-        }
+    private void save(String filename) {
+
     }
 
-    public void listByType(String typeName) {
+    public void list(String typeName) {
         System.out.println("Objects of type '" + typeName + "':");
 
         boolean found = false;
@@ -303,7 +285,7 @@ public class Interpreter {
      * }
      */
 
-    public void GrowBody(int threadid) {
+    public void growbody(int threadid) {
         GameObject mushroomthread = game.getObject(threadid);
         if (mushroomthread instanceof MushroomThread mt) {
             try {
