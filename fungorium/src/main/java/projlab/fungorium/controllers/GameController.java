47bb2f4d41678dddb018be04ec4d1922bf9d60d7
CCColda <cccolda@gmail.com>
2025-05-13 12:33:13 +0200

    GameComponentViewVisitor interface & empty impl

--- fungorium/src/main/java/projlab/fungorium/controllers/GameController.java
+++ fungorium/src/main/java/projlab/fungorium/controllers/GameController.java
@@ -1,7 +1,42 @@
 package projlab.fungorium.controllers;
 
 import projlab.fungorium.interfaces.GameComponentViewVisitor;
+import projlab.fungorium.views.gamecomponents.InsectView;
+import projlab.fungorium.views.gamecomponents.MushroomBodyView;
+import projlab.fungorium.views.gamecomponents.SporeView;
+import projlab.fungorium.views.gamecomponents.TectonView;
+import projlab.fungorium.views.gamecomponents.ThreadView;
 
 public class GameController implements GameComponentViewVisitor {
+
+	@Override
+	public void visit(TectonView tectonView) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'visit'");
+	}
+
+	@Override
+	public void visit(ThreadView threadView) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'visit'");
+	}
+
+	@Override
+	public void visit(InsectView insectView) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'visit'");
+	}
+
+	@Override
+	public void visit(MushroomBodyView mushroomBodyView) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'visit'");
+	}
+
+	@Override
+	public void visit(SporeView sporeView) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'visit'");
+	}
 
 }
