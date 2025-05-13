3b49dd6415bb9f8821c8021cc8ea69ef35ebff0e
Szmoleniczki Ákos <145291965+szmoli@users.noreply.github.com>
2025-05-13 16:22:53 +0200

    TectonView draw

--- fungorium/src/main/java/projlab/fungorium/views/gamecomponents/TectonView.java
+++ fungorium/src/main/java/projlab/fungorium/views/gamecomponents/TectonView.java
@@ -6,17 +6,24 @@ import projlab.fungorium.interfaces.GameComponentViewVisitor;
 import projlab.fungorium.models.Tecton;
 
 public class TectonView extends GameComponentView<Tecton> {
-
+    // Ezt nem kellene priváttá tenni?
+    public String type;
+    
     @Override
     public void draw(Graphics2D g) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+        g.drawOval(center.x, center.y, CELL_SIZE, CELL_SIZE);
+
+        // TODO: ezek fogalmam sincs, hogy tényleg jók-e majd le kellene ellenőrizni, de fejben nekem kijön a matek
+        // X tengelyen a cellában középre legyen igazítva a text
+        int textStartX = center.x - (type.length() / 2);
+        // Y tengelyen a cella aljához legyen igazítva a text
+        // TODO: nem tudom, hogy az Y tengely melyik irányba pozitív itt, de gondolom lefele (ezért + és nem - az offset)?
+        int textStartY = center.y + (CELL_SIZE / 2);
+        g.drawString(type, textStartX, textStartY);
     }
 
     @Override
     public void accept(GameComponentViewVisitor visitor) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'accept'");
+        
     }
-
 }
