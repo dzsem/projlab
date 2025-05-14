ce1c7a521dc080798e2693969959f518c985cabb
Paál Péter Ferenc <paalpeter04@gmail.com>
2025-05-14 08:49:22 +0200

    added getSelectedBody to MycologistController, corrected DistributeSpore to use it

--- fungorium/src/main/java/projlab/fungorium/actions/mycologist/DistributeSporeAction.java
+++ fungorium/src/main/java/projlab/fungorium/actions/mycologist/DistributeSporeAction.java
@@ -5,22 +5,25 @@ import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 
 import projlab.fungorium.controllers.GameController;
+import projlab.fungorium.controllers.MycologistController;
 import projlab.fungorium.models.MushroomBody;
 
 public class DistributeSporeAction extends AbstractAction {
     
     private GameController controller;
+    private MycologistController mycologistcontroller;
 
-    public DistributeSporeAction(GameController controller) {
+    public DistributeSporeAction(GameController controller, MycologistController mycologistcontroller) {
         super();
 
         this.controller = controller;
+        this.mycologistcontroller = mycologistcontroller;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        MushroomBody mb = controller.getSelectedBody();
         try {
+            MushroomBody mb = mycologistcontroller.getSelectedBody();
             mb.distributeSpores();
         } catch (Exception ex) {
             controller.showError(ex);
