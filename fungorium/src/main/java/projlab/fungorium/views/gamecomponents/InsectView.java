b6e95e68734b0b3eb5322bb19ef4efa8a772313f
Szmoleniczki Ákos <145291965+szmoli@users.noreply.github.com>
2025-05-15 13:33:08 +0200

    InsectView wip

--- fungorium/src/main/java/projlab/fungorium/views/gamecomponents/InsectView.java
+++ fungorium/src/main/java/projlab/fungorium/views/gamecomponents/InsectView.java
@@ -1,22 +1,22 @@
 package projlab.fungorium.views.gamecomponents;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 import projlab.fungorium.interfaces.GameComponentViewVisitor;
 import projlab.fungorium.models.Insect;
 
 public class InsectView extends GameComponentView<Insect> {
-
+    // TODO: Tectonról lekérdezni, hogy hány Insect van összesen és a GameControllertől kapott sorszám alapján kiszámolni rá egy offsetet.
     @Override
     public void draw(Graphics2D g) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+        g.drawImage(imagePath, null, center.x, center.y);
     }
 
     @Override
     public void accept(GameComponentViewVisitor visitor) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'accept'");
+        visitor.visit(this);
     }
 
+    private BufferedImage imagePath;
 }
