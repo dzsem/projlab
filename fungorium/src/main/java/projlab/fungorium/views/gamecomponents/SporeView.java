14b59b10744215abc5132207fa3f26711102ede8
Szmoleniczki Ákos <145291965+szmoli@users.noreply.github.com>
2025-05-15 13:40:15 +0200

    SporeView wip

--- fungorium/src/main/java/projlab/fungorium/views/gamecomponents/SporeView.java
+++ fungorium/src/main/java/projlab/fungorium/views/gamecomponents/SporeView.java
@@ -1,22 +1,22 @@
 package projlab.fungorium.views.gamecomponents;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 import projlab.fungorium.interfaces.GameComponentViewVisitor;
 import projlab.fungorium.models.MushroomSpore;
 
 public class SporeView extends GameComponentView<MushroomSpore> {
-
+    // TODO: Tektonon belüli offset számolás, mint az InsectViewnál.
     @Override
     public void draw(Graphics2D g) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+        g.drawImage(imagePath, null, center.x, center.y);
     }
 
     @Override
     public void accept(GameComponentViewVisitor visitor) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'accept'");
+        // TODO: Ez kell egyáltalán? Nem is csináltunk rá visitort.
     }
 
+    private BufferedImage imagePath;
 }
