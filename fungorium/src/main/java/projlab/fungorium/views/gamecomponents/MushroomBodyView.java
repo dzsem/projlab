f9410d7dc69866c3ffc4d583b5f30f7b7c39e0cd
Szmoleniczki Ákos <145291965+szmoli@users.noreply.github.com>
2025-05-15 13:35:13 +0200

    MushroomBodyView

--- fungorium/src/main/java/projlab/fungorium/views/gamecomponents/MushroomBodyView.java
+++ fungorium/src/main/java/projlab/fungorium/views/gamecomponents/MushroomBodyView.java
@@ -1,22 +1,21 @@
 package projlab.fungorium.views.gamecomponents;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 import projlab.fungorium.interfaces.GameComponentViewVisitor;
 import projlab.fungorium.models.MushroomBody;
 
 public class MushroomBodyView extends GameComponentView<MushroomBody> {
-
     @Override
     public void draw(Graphics2D g) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+        g.drawImage(imagePath, null, center.x, center.y);
     }
 
     @Override
     public void accept(GameComponentViewVisitor visitor) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'accept'");
+        visitor.visit(this);
     }
 
+    private BufferedImage imagePath;
 }
