3952efe99e4aea5c21b0326ea42e6f3d2ca37532
Szmoleniczki Ákos <szmoleniczki.akos@edu.bme.hu>
2025-05-16 15:00:12 +0200

    CELL_SIZE -> size

--- fungorium/src/main/java/projlab/fungorium/views/gamecomponents/TectonView.java
+++ fungorium/src/main/java/projlab/fungorium/views/gamecomponents/TectonView.java
@@ -11,14 +11,14 @@ public class TectonView extends GameComponentView<Tecton> {
     
     @Override
     public void draw(Graphics2D g) {
-        g.drawOval(center.x, center.y, CELL_SIZE, CELL_SIZE);
+        g.drawOval(center.x, center.y, size.x, size.y);
 
         // TODO: ezek fogalmam sincs, hogy tényleg jók-e majd le kellene ellenőrizni, de fejben nekem kijön a matek
         // X tengelyen a cellában középre legyen igazítva a text
         int textStartX = center.x - (type.length() / 2);
         // Y tengelyen a cella aljához legyen igazítva a text
         // TODO: nem tudom, hogy az Y tengely melyik irányba pozitív itt, de gondolom lefele (ezért + és nem - az offset)?
-        int textStartY = center.y + (CELL_SIZE / 2);
+        int textStartY = center.y + (size.y / 2);
         g.drawString(type, textStartX, textStartY);
     }
 
