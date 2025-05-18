381fa5c536668dad56becf2066374e106de39d29
CCColda <cccolda@gmail.com>
2025-05-18 13:42:03 +0200

    PassAction bugfix, updatable SidePanel

--- fungorium/src/main/java/projlab/fungorium/windowing/game/SidePanel.java
+++ fungorium/src/main/java/projlab/fungorium/windowing/game/SidePanel.java
@@ -2,11 +2,11 @@ package projlab.fungorium.windowing.game;
 
 import java.awt.Dimension;
 import java.util.List;
+import java.util.Map;
 
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-
 
 public class SidePanel extends JPanel {
     public static final int WIDTH = 250;
@@ -15,13 +15,19 @@ public class SidePanel extends JPanel {
     public SidePanel(List<AbstractAction> actions) {
         setPreferredSize(new Dimension(WIDTH, 0));
 
+        update(actions);
+    }
+
+    public void update(List<AbstractAction> actions) {
         if (actions == null) {
             return;
         }
 
+        removeAll();
+
         for (AbstractAction action : actions) {
             JButton btn = new JButton(action);
-            btn.setPreferredSize(new Dimension(WIDTH, BTN_HEIGHT)); 
+            btn.setPreferredSize(new Dimension(WIDTH, BTN_HEIGHT));
             add(btn);
         }
     }
