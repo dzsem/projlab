c07375da49c8880c026af8a73081c1e2bf4aaf23
CCColda <cccolda@gmail.com>
2025-05-18 15:12:19 +0200

    Gitignore, update script

--- scripts/createRunCommand.ps1
+++ scripts/createRunCommand.ps1
@@ -1,8 +1,33 @@
-$ROOT_FOLDER = "$PSScriptRoot\..\fungorium"
-$BASE_PATH = "$ROOT_FOLDER\src\main\java\projlab\fungorium"
+#! powershell
+# használat: createRunCommand.ps1 -out "buildAndRun.bat" [-in "fungorium-mappa"]
+
+param(
+	[string] $in="$PSScriptRoot\..\fungorium",
+	[string] $out=""
+)
+
+function Print {
+	param(
+		[string]$line
+	)
+
+	if ($out -eq "") {
+		Write-Output $line
+	}
+	else {
+		Add-Content -Path $out -Value $line -Encoding Ascii
+	}
+}
+
+if ($out -ne "" -and (Test-Path $out -PathType Leaf)) {
+	Clear-Content -Path $out
+}
+
+$BASE_PATH = "$in\src\main\java\projlab\fungorium"
+$RESOURCE_PATH = "$in\src\main\resources"
 $BUILD_PATH = ".\build"
 
-$all_java_files = @(Get-ChildItem "$ROOT_FOLDER\**\*.java" -Recurse)
+$all_java_files = @(Get-ChildItem "$in\**\*.java" -Recurse)
 $folders = @(".")
 
 $prev_location = Get-Location
@@ -16,17 +41,18 @@ foreach ($java_file in $all_java_files) {
 	}
 }
 
-$manifest_relative_path = "..\$(Resolve-Path -Path "$ROOT_FOLDER\manifest.txt" -Relative)"
+$manifest_relative_path = "..\$(Resolve-Path -Path "$in\manifest.txt" -Relative)"
 $joined_folders = ($folders | % {"$_\*.java"}) -join ' '
 
 Set-Location $prev_location
 
-Write-Output "cd `"$(Resolve-Path -Path $BASE_PATH -Relative)`""
-Write-Output ""
-Write-Output "javac -d `"$BUILD_PATH`" $joined_folders"
-Write-Output ""
-Write-Output "cd `"$BUILD_PATH`""
-Write-Output ""
-Write-Output "jar -cmvf $manifest_relative_path fungorium.jar *"
-Write-Output ""
-Write-Output "java -jar fungorium.jar"
+Print "rem Run in: cmd"
+Print ""
+Print "cd `"$(Resolve-Path -Path $BASE_PATH -Relative)`""
+Print "javac -d `"$BUILD_PATH`" $joined_folders"
+Print "cd `"$BUILD_PATH`""
+Print "xcopy $RESOURCE_PATH . /E /C /I /Y"
+Print "jar -cmvf $manifest_relative_path fungorium.jar *"
+Print ""
+Print "java -jar fungorium.jar"
+Print "exit /B"
