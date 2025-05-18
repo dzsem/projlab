b3757f833131ad3fe4b09c771fedd7813395471c
CCColda <cccolda@gmail.com>
2025-05-18 13:21:24 +0200

    Image loader patch

--- fungorium/src/main/java/projlab/fungorium/views/gamecomponents/ThreadView.java
+++ fungorium/src/main/java/projlab/fungorium/views/gamecomponents/ThreadView.java
@@ -1,11 +1,8 @@
 package projlab.fungorium.views.gamecomponents;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -18,10 +15,10 @@ public class ThreadView extends GameComponentView<MushroomThread> {
     private static final int MUSHROOMTHREAD_DRAWPRIORITY = 2;
     public static final double RADIUS_MULTIPLIER = 0.7;
     private static final Point MUSHROOMTHREAD_SIZE = new Point(32, 32);
-    private static final String sprout1Path = "images\\SproutThread1.png";
-    private static final String sprout2Path = "images\\SproutThread1.png";
-    private static final String grownPath = "images\\Thread.png";
-    private static final String cutPath = "images\\CutThread.png";
+    private static final String SPROUT1_Path = "images/SproutThread1.png";
+    private static final String SPROUT2_Path = "images/SproutThread1.png";
+    private static final String GROWN_PATH = "images/Thread.png";
+    private static final String CUT_PATH = "images/CutThread.png";
 
     private BufferedImage image;
 
@@ -34,19 +31,18 @@ public class ThreadView extends GameComponentView<MushroomThread> {
         super(gameObject, center, MUSHROOMTHREAD_SIZE);
 
         try {
-            sprout1Image = ImageIO.read(new File(sprout1Path));
-            sprout2Image = ImageIO.read(new File(sprout2Path));
-            grownImage = ImageIO.read(new File(grownPath));
-            cutImage = ImageIO.read(new File(cutPath));
+            sprout1Image = ImageIO.read(getClass().getClassLoader().getResourceAsStream(SPROUT1_Path));
+            sprout2Image = ImageIO.read(getClass().getClassLoader().getResourceAsStream(SPROUT2_Path));
+            grownImage = ImageIO.read(getClass().getClassLoader().getResourceAsStream(GROWN_PATH));
+            cutImage = ImageIO.read(getClass().getClassLoader().getResourceAsStream(CUT_PATH));
         } catch (IOException e) {
             e.printStackTrace();
-        } 
+        }
     }
 
-    // TODO: Ennél is megoldani a tektonon belüli eltolást, mint az InsectViewnál.
     @Override
     public void draw(Graphics2D g) {
-        MushroomThread thread = getGameObject(); 
+        MushroomThread thread = getGameObject();
         if (thread.getCutState() == CutState.CUT) {
             image = cutImage;
         }
@@ -58,20 +54,20 @@ public class ThreadView extends GameComponentView<MushroomThread> {
                         image = sprout1Image;
                     } else if (thread.getTurnsToGrow() > 1) {
                         image = sprout2Image;
-                    }     
+                    }
                     break;
 
                 case GROWN:
                     image = grownImage;
                     break;
-            
+
                 default:
                     break;
             }
         }
 
         if (image != null) {
-            g.drawImage(image, center.x - size.x / 2, center.y - size.y / 2, size.x, size.y, null );
+            g.drawImage(image, center.x - size.x / 2, center.y - size.y / 2, size.x, size.y, null);
         }
     }
 
